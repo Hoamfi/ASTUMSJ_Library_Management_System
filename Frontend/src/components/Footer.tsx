333a89af7635ad78e191fab7e0910fa157737f12
Ammar Sabit <ammarsabit39@gmail.com>
2025-08-25 17:50:34 +0300

    Build a minimal Footer

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,7 +1,10 @@
 const Footer = () => {
   return (
-    <div className='border-1 border-gray-200 '>Footer</div>
-  )
-}
+    <div className="border-1 border-gray-200 w-full p-3 flex flex-col align-center  md:flex-row md:justify-center">
+      <p className="font-sans self-center text-sm text-gray-700">ASTUMSJ Library </p>
+      <p className="self-center text-sm text-gray-700"> &copy; 2025. All rights reserved.</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
