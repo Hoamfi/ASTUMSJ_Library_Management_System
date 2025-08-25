75b2339141524663e9fe6f604f049d6d2af3fed3
Ammar Sabit <ammarsabit39@gmail.com>
2025-08-26 01:25:24 +0300

    Fix a bug related to authorization

--- Backend/src/middleware/admin.ts
+++ Backend/src/middleware/admin.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
 export default function isAdmin(req: Request, res: Response, next: () => void) {
-  if (!(req as any).user.admin) return res.status(403).send("Access denied.");
+  if (!(req as any).user.isAdmin) return res.status(403).send("Access denied.");
   next();
 }
--- Backend/src/routes/book.ts
+++ Backend/src/routes/book.ts
@@ -7,13 +7,14 @@ import {
   deleteBook,
 } from "../controllers/bookController";
 import isAdmin from "../middleware/admin";
+import auth from "@/middleware/auth";
 
 const bookRouter = Router();
 
-bookRouter.post("/", isAdmin, createBook); // admin only
-bookRouter.get("/", getBooks); // public
+bookRouter.post("/", auth, isAdmin, createBook); // admin only
+bookRouter.get("/", auth, isAdmin, getBooks); // public
 bookRouter.get("/:id", getBookById); // public
-bookRouter.put("/:id", isAdmin, updateBook); // admin only
-bookRouter.delete("/:id", isAdmin, deleteBook); // admin only
+bookRouter.put("/:id", auth, isAdmin, updateBook); // admin only
+bookRouter.delete("/:id", auth, isAdmin, deleteBook); // admin only
 
 export default bookRouter;
