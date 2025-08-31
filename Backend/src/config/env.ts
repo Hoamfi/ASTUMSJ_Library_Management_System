b21c3b8e8fb5fe128e035ba19fa107fcb5c1c7ea
hos <hosamaadem8@gmail.com>
2025-08-31 08:23:49 +0300

    Delete Backend/src/config/env.ts

--- Backend/src/config/env.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const ENV = {
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || "dev_secret_key",
-  PORT: process.env.PORT || 3000,
-  ATLAS_URL: "mongodb+srv://hostech888_db_user:o6zTIg7gnO7Fs8mX@cluster0.fq3oh1t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-};
