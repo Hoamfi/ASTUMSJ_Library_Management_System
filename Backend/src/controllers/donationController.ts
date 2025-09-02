1beb7b9bfab71564b5e15ac967ec60b9589b4812
Hosama Adem <hosamaadem8@gmail.com>
2025-09-02 22:52:42 +0300

    student submission for donation api

---
+++ Backend/src/controllers/donationController.ts
@@ -0,0 +1,21 @@
+import { Request,Response } from "express";
+import Donation,{IDonation} from "../models/donationModel";
+
+//POST /api/donations
+export const createDonation = async(req:Request,res:Response):Promise<void> => {
+    try{
+        const userId=(req as {user?: {id:string}}).user?.id;
+
+        const donation= await Donation.create({
+            user:userId,
+            amount:req.body.amount,
+            reference:req.body.amount,
+            screenshot:req.body.screenshot,
+            status:"Pending",
+        });
+        res.status(201).json({ message: "Donation submitted", donation });
+    } catch(error:any){
+        res.status(400).json({ error: error.message });
+    }   
+};
+
