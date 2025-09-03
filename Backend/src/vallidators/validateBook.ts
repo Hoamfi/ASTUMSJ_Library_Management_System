0cfa54da9b1beef861faff4accbd5e6f185f7432
Hosama Adem <hosamaadem8@gmail.com>
2025-09-03 18:37:08 +0300

    validation of books form

--- Backend/src/vallidators/validateBook.ts
+++ Backend/src/vallidators/validateBook.ts
@@ -1,13 +1,16 @@
-import Joi from "joi";
+import {z} from "zod";
 
-interface Book {
+export const bookSchema = z.object({
+    title: z.string().min(2,"Title must be at least 2 characters").max(150,"Title is too long"),
+    author: z.string().min(2,"Author name must be at last 2 characters").max(100,"Author name is too long"),
+    publicationYear:z.number().int("publicationYear must be an integer").gte(1000,"publicationYear must be realstic").lte(new Date().getFullYear(),"publicationYear can not be in the future"),
+    bookCover:z.string().url("bookCover must be valid url"),
+    borrowCount:z.number().int().min(0,"borrow count can't be negative "),
+})
 
-}
+//inference
+export type BookInput = z.infer<typeof bookSchema>;
 
-export default function validateBook(book: Book) {
-    const schema = Joi.object({
-
-    })
-
-    return schema.validate(book)
+export default function validateBook(book: unknown) {
+  return bookSchema.safeParse(book);
 }
--- Backend/src/vallidators/validateStudent.ts
+++ Backend/src/vallidators/validateStudent.ts
@@ -1,13 +1,20 @@
-import {z} from "zod";
+import { z } from "zod";
 
 export const studentSchema = z.object({
-    name:z.string().min(5,"Name must be at least 5 characters").max(200,"Name is too long"),
-    email:z.string().email("Invalid email format"),
-    password:z.string().min(8,"password must be at least 8 character").max(200,"password is too long "),
+  name: z
+    .string()
+    .min(5, "Name must be at least 5 characters")
+    .max(200, "Name is too long"),
+  email: z.string().email("Invalid email format"),
+  password: z
+    .string()
+    .min(8, "password must be at least 8 character")
+    .max(200, "password is too long "),
 });
 
+//inference
 export type StudentInput = z.infer<typeof studentSchema>;
 
-export default function validateStudent(student:unknown){
-    return studentSchema.safeParse(student);
+export default function validateStudent(student: unknown) {
+  return studentSchema.safeParse(student);
 }
