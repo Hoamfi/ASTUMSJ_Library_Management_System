a9cd5fd9cc50ec85f90cbabd0522467155a49450
Ammar Sabit <ammarsabit7@gmail.com>
2025-09-06 14:48:43 +0300

    Integrate admin dashboard's books by catagory. Add generate reports functionality

--- Frontend/src/pages/AdminPages/Dashboard.tsx
+++ Frontend/src/pages/AdminPages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   LineChart,
   Line,
@@ -15,6 +15,8 @@ import {
   Legend,
 } from "recharts";
 import apiClient from "../../services/api-client";
+import logo from "../../assets/logo-with-bg.jpg";
+import jsPDF from "jspdf";
 
 const borrowingTrends = [
   { month: "Jan", borrowings: 50 },
@@ -23,12 +25,6 @@ const borrowingTrends = [
   { month: "Apr", borrowings: 120 },
   { month: "May", borrowings: 65 },
   { month: "Jun", borrowings: 150 },
-];
-
-const booksByCategory = [
-  { name: "Islamic", value: 400 },
-  { name: "Self Helps", value: 300 },
-  { name: "Bussiness", value: 200 },
 ];
 
 const topBooks = [
@@ -52,41 +48,86 @@ function StatCard({ title, value }: { title: string; value: number }) {
 }
 
 export default function AdminDashboard() {
+  const pdfRef = useRef(null);
+  // stats
   const [totalBooks, setTotalBooks] = useState(0);
   const [totalUsers, setTotalUsers] = useState(0);
   const [totalBorrows, setTotalBorrows] = useState(0);
   const [totalDonations, setTotalDonations] = useState(0);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const headers = { "x-auth-token": token };
+  // books
+  const [islamic, setIslamic] = useState(0);
+  const [self, setSelf] = useState(0);
+  const [bussiness, setBussiness] = useState(0);
+  const booksByCategory = [
+    { name: "Islamic", value: islamic },
+    { name: "Self Helps", value: self },
+    { name: "Bussiness", value: bussiness },
+  ];
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const headers = { "x-auth-token": token };
+    const fetchStats = async () => {
+      try {
         const [booksRes, usersRes, borrowsRes, donationsRes] =
           await Promise.all([
             apiClient.get("/books", { headers }),
             apiClient.get("/students/allCount", { headers }),
             apiClient.get("/borrow/", { headers }),
-            apiClient.get("/donations/admin/all", { headers }),
+            // apiClient.get("/donations/admin/all", { headers }),
           ]);
 
         setTotalBooks(booksRes.data.count);
         setTotalUsers(usersRes.data);
         setTotalBorrows(borrowsRes.data.count);
-        setTotalDonations(donationsRes.data.count);
+        // setTotalDonations(donationsRes.data.count);
       } catch (err) {
         console.error(err);
       }
     };
 
-    fetchData();
+    const fetchBooks = async () => {
+      try {
+        const [islamic, self, bussiness] = await Promise.all([
+          apiClient.get("/books/?catagory=islamic", { headers }),
+          apiClient.get("/books/?catagory=self", { headers }),
+          apiClient.get("/books/?catagory=bussiness", { headers }),
+        ]);
+
+        setIslamic(islamic.data.count);
+        setSelf(self.data.count);
+        setBussiness(bussiness.data.count);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchStats();
+    fetchBooks();
   }, []);
+
+  const generateReport = () => {
+    const doc = new jsPDF();
+    doc.text("ASTUMSJ Library Report", 75, 10);
+    doc.text(`Total Books: ${totalBooks} `, 60, 20);
+    doc.text(`Total Members: ${totalUsers} `, 60, 28);
+    doc.text(`Total Donation: ${totalDonations}Birr `, 60, 35);
+    doc.text("Books by Catagory:", 60, 42);
+    doc.text(`Islamic: ${islamic} `, 65, 50);
+    doc.text(`Self Helps: ${self} `, 65, 58);
+    doc.text(`Bussiness: ${bussiness} `, 65, 66);
+
+    doc.save("Report.pdf");
+  };
   return (
     <div className="flex-1 p-8 space-y-8">
       <div className="flex justify-between items-center">
         <h2 className="text-3xl font-bold">Dashboard Overview</h2>
-        <button className="bg-black dark:bg-[#1d293d] text-white px-4 py-2 rounded-lg shadow hover:bg-black/80 dark:hover:bg-[#1d293d]/80 cursor-pointer">
+        <button
+          className="bg-black dark:bg-[#1d293d] text-white px-4 py-2 rounded-lg shadow hover:bg-black/80 dark:hover:bg-[#1d293d]/80 cursor-pointer"
+          onClick={() => generateReport()}
+        >
           Generate Report
         </button>
       </div>
@@ -102,7 +143,11 @@ export default function AdminDashboard() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         <div className="dark:bg-[#1d293d] p-6 rounded-2xl shadow">
           <h3 className="text-lg font-semibold mb-4">📈 Borrowing Trends</h3>
-          <ResponsiveContainer width="100%" height={300}>
+          <ResponsiveContainer
+            width="100%"
+            height={300}
+            className="dark:text-black"
+          >
             <LineChart data={borrowingTrends}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="month" />
