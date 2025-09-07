9749b35f8d0ae69505866a0be5ef683810a6ed28
Hosama Adem <hosamaadem8@gmail.com>
2025-09-07 08:39:41 +0300

    borrow route

--- Backend/src/routes/borrowRoutes.ts
+++ Backend/src/routes/borrowRoutes.ts
@@ -9,36 +9,27 @@ import {
   approveReturnBook,
   getStudentBorrowHistory,
   borrowBookApproved,
-<<<<<<< HEAD
   getPendingRequests,
 
-=======
->>>>>>> 2c8976bcd9baa1790c5e6102353e26256fb02dc3
+
+
 } from "../controllers/borrowController";
 
 const router = express.Router();
 
-<<<<<<< HEAD
-router.post("/:bookId",  borrowBook);
-router.put("/return/:borrowId", auth, returnBook);
-router.get("/me/studentId", getMyBorrows);
 
-router.get("/", [auth, isAdmin], getAllBorrows);
-router.put("/admin/approvereturn/:borrowId",approveReturnBook);
-router.get("/admin/:studentId",getStudentBorrowHistory);
-router.put("/borrowapproved/:bookId",borrowBookApproved);
-router.get("/admin/pendings",getPendingRequests);
-=======
+
 // user
 router.post("/:bookId", auth, borrowBook);
 router.put("/return/:borrowId", auth, returnBook);
 router.get("/myborrows", auth, getMyBorrows);
->>>>>>> 2c8976bcd9baa1790c5e6102353e26256fb02dc3
+
 
 // admin
 router.get("/admin/allborrows", [auth, isAdmin], getAllBorrows);
 router.get("/admin/borrowhistory/:studentId", auth, isAdmin, getStudentBorrowHistory);
 router.put("/admin/approvereturn/:borrowId", auth, isAdmin, approveReturnBook);
 router.put("/borrowapproved/:bookId", auth, isAdmin, borrowBookApproved);
+router.get("/admin/pendings",getPendingRequests );
 
 export default router;
